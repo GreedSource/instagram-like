e4a972706b1d5f5d8599a39a96859465de9c3345
GreedSource <greedsource@outlook.com>
2021-07-18 11:34:00 -0500

    heroku deploy preparation

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config()
 const express = require('express')
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT || 3700
 require('./models/index')
 
 //routes
@@ -13,6 +13,15 @@ app.use(express.json())
 app.use('/api/auth', routes.Auth)
 app.use('/api/post', routes.Post)
 app.use('/api/user', routes.User)
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'))
+    const path = require('path')
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    })
+}
+
 
 app.listen(port, () => {
     console.log(`Server is running in port: http://localhost:${port}`)
